04c539e43acdd2d3e5b69a7b135157ccd5e76737
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-17 11:49:15 -0800

    printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - printing all single digits without using  particular type of variable
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
