1c00f06d9a1ccc9c576b7a99edfa4c9e5a0572bd
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-17 11:26:36 -0800

    printing all alphabets in lower case minus some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - printing all the alphabets in lower case minus q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
