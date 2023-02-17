2d782ec17eaf5bb138e39c2f4cf1c890f7b87fb3
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-17 11:37:49 -0800

    printing all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - printing all single digits of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
