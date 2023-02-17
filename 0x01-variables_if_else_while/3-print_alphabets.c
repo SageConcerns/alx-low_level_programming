5832f7740aaf647bc57b5971aea22c2d0b43353d
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-17 11:16:36 -0800

    printing the alphabet in lower case and then upper case letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - printing the English alphabet in lower case and then the uppercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
