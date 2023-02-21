722a0fc19835bc05d433676fa1ab7b6845fb43db
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-21 15:36:35 -0800

    checing for alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#incluew "mai.h"
+
+/**
+ * _isalpha - checks for char alphabet
+ * @c: the char to be checked
+ *
+ * Return: 1 if char is a letter, lower or upper case, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c <= 'A' && c >= 'Z'))
+		return (1);
+	else
+		return (0);
+}
