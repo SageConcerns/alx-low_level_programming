a893de7b432ad1edc073c9cc15d67205838f4b9a
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-21 15:00:47 -0800

    checking for upper case letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _is lower - checkining char for lower case
+ * Return: 1 for lower case, otherwise 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
