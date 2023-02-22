e2dfa384ccdd2ab73f59e737e4cf646e485e70ae
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-21 17:24:03 -0800

    finding absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of integer
+ * n - the integer
+ * Return: the abs of the integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
