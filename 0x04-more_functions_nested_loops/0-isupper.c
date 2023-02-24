f2266cbfa31914b0a338e63a2a68a672a06becbf
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-23 16:02:26 -0800

    checking uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - return 1 if c is upper and o if otherwise
+ * @c: character to be checked
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 and c<= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
