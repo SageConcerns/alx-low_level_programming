35e79a9a602ad458e373fde5fea75f08c17aaff7
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-27 04:44:11 -0800

    returing length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
  * _strlen - returns length of string
- * *s: string to be checked
+ * @s: char to check
  * Return: 0
  */
 
 int _strlen(char *s)
 {
 	int a = 0; /*start counter from 0*/
-	
+
 	for (; *s++;)
 		a++;
 	return (a);
