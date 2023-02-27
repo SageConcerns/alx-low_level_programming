fecfe81d1b71b6f0f1849d82a469e43f130c258b
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-27 04:58:26 -0800

    printing a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prings a string followed by new line
+ * @str: the string
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	-putchar('\n');
+}
