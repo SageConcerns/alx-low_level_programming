d3d1438348bb81b46c6e0424869317751491debc
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-02-27 05:37:19 -0800

    printing a string in reverse followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - printing in reverse
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+
+	while (s[n] != '\n')
+		b++;
+
+	for (a = b - 1; a >= 0; i--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
