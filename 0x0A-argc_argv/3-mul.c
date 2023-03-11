355309691cb5cbdcfd7bd577941c7f4eb4caa748
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-03-10 17:33:18 -0800

    argc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digits;
+	int i, d, n, len, f, digit;
 
 	i = 0;
 	d = 0;
@@ -26,22 +26,22 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-		if (s[i] >= 0 && s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 + digits;
-			f - 1;
-			if (s[i + 1] < 'a' || s[i + 1] > '9')
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
 		i++;
 	}
 	if (f == 0)
-
 		return (0);
+
 	return (n);
 }
 
@@ -51,11 +51,13 @@ int _atoi(char *s)
  * @argv: array of arguments
  *
  * Return: 0 (Success), 1 (Error)
+ *
+ * Return: 0 (Success), 1 (Error)
  */
 
 int main(int argc, char *argv[])
 {
-	it result, num1, num2;
+	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
@@ -65,7 +67,8 @@ int main(int argc, char *argv[])
 
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-	result = num1 + num2
+	result = num1 * num2;
+
 	printf("%d\n", result);
 
 	return (0);
