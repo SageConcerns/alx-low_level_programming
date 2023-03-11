1b2eb9342d6d8603e5f321b209085a8bf74da0e3
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-03-10 18:18:41 -0800

    argc and argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -8,7 +10,7 @@
  * Return: 0 (Success), 1 (Error)
  */
 
-int main(int argc, char *arv[])
+int main(int argc, char *argv[])
 {
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
@@ -18,20 +20,22 @@ int main(int argc, char *arv[])
 	printf("Error\n");
 	return (1);
 	}
+
 	num = atoi(argv[1]);
 	result = 0;
+
 	if (num < 0)
 	{
-	printf("0\n");
-	return (0);
+		printf("0\n");
+		return (0);
 	}
 	for (j = 0; j < 5 && num >= 0, j++)
 	{
-	while (num >= coins[j])
-	{
-	result++
-	num -= coins[j];
-	}
+		while (num >= coins[j])
+		{
+			result++
+			num -= coins[j];
+		}
 	}
 	printf("%d\n", result);
 	return (0);
