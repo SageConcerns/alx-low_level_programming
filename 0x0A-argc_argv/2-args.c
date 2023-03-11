530d1ca345389aa1299b604df8fa6debdae40442
SageConcerns <okwuanaso.f.trueconnect@gmail.com>
2023-03-10 17:16:12 -0800

    argc and argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,14 +6,14 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0, i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
